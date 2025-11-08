eeef043f9a6cdcd2ba37b92cb88592f1f066d028
MargineanMiruna-Elena <miruna.marginean.2018@gmail.com>
2025-11-08 19:22:44 +0200

    auth repo

--- app/src/main/java/com/mat/mindpet/repository/AuthRepository.java
+++ app/src/main/java/com/mat/mindpet/repository/AuthRepository.java
@@ -25,22 +25,5 @@ public class AuthRepository {
         void onFailure(String errorMessage);
     }
 
-    public void signUp(String email, String password, String firstName, String lastName, AuthCallback callback) {
-        auth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(task -> {
-                    if (task.isSuccessful()) {
-                        FirebaseUser firebaseUser = auth.getCurrentUser();
 
-                        if (firebaseUser != null) {
-                            String userId = firebaseUser.getUid();
-                            String joinDate = LocalDate.now().format(DateTimeFormatter.ISO_LOCAL_DATE);
-                            User newUser = new User(userId, firstName, lastName, email, password, joinDate, null);
-
-                            
-                        }
-                    } else {
-                        callback.onFailure(task.getException().getMessage());
-                    }
-                });
-    }
 }
